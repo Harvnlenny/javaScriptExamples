d90a5421a9dbf1d7c7e67218a10de2d0d966cd28
Aaron Harvey <harvnlenny@gmail.com>
2016-04-02 10:30:07 -0400

    check user guess

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -8,4 +8,13 @@ var isSunk = false;
 
 while (isSunk == false) {
     guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses++;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits++;
+        }
+    }
 }
+
