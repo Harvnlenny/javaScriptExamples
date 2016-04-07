59b8205563277105bbabd0f35bf4ddbfe4df24a0
Aaron Harvey <harvnlenny@gmail.com>
2016-04-06 20:18:58 -0400

    Implementing the model object, setting up the fire method, and look for hits

--- game/battleship.js
+++ game/battleship.js
@@ -13,7 +13,47 @@ var view = {
     }
 };
 
-view.displayMiss("00");
-view.displayHit("34");
 
-view.displayMessage("Tap tap, is this thing on?");
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipsSunk: 0,
+    shipLength: 3,
+    ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] },
+             { locations: ["24", "34", "44"], hits: ["", "", ""] },
+             { locations: ["10", "11", "12"], hits: ["", "", ""] } ],
+
+    fire: function(guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            var locations = ship.locations;
+            var index = locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+    isSunk: function(ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+
+};
+
+model.fire("53");
+
+model.fire("06");
